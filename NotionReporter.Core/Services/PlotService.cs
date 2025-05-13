8623851847978333b24e56b49a9dba579114be4a
stephen-cripps <stephen.cripps@tutanota.com>
2025-05-13 23:00:08 +0100

    Update PlotService.cs

--- NotionReporter.Core/Services/PlotService.cs
+++ NotionReporter.Core/Services/PlotService.cs
@@ -5,12 +5,12 @@ using ScottPlot;
 
 public static class PlotService
 {
-    public static void GeneratePlots(List<Member> members, List<Event> events,  string plotFolder)
+    public static void GeneratePlots(List<Member> members, List<Event> events, string plotFolder)
     {
         var memberPlotData = members
             .Select(m => new MemberPlotData(
                     m.Name ?? "",
-                    GetAttendance(m, events,0, 6),
+                    GetAttendance(m, events, 0, 6),
                     GetAttendance(m, events, 6, 12)
                 )
             ).ToList();
@@ -18,6 +18,7 @@ public static class PlotService
         PlotAttendance(memberPlotData, plotFolder);
         PlotChangeInAttendance(memberPlotData, plotFolder);
         PlotMembersMeetingAttendance(events, plotFolder);
+        PlotTemporalAttendance(events, plotFolder);
     }
 
     private static void PlotAttendance(List<MemberPlotData> members, string plotFolder)
@@ -71,16 +72,15 @@ public static class PlotService
         plot.Legend.Orientation = Orientation.Horizontal;
         plot.ShowLegend(Alignment.UpperRight);
 
-        // tell the plot to autoscale with no padding beneath the bars
         plot.Axes.Margins(bottom: 0, top: .3);
-        
+
         plot.YLabel("Events Attended");
         plot.Title("Events Attended by Members");
 
         var formattedDate = DateTime.Now.ToString("yyyy-MM-dd");
-        
+
         var filePath = Path.Combine(plotFolder, $"attendance_{formattedDate}.png");
-        
+
         plot.SavePng(filePath, 1800, 800);
     }
 
@@ -94,7 +94,7 @@ public static class PlotService
         var plot = new Plot();
         IPalette amberPalette = new ScottPlot.Palettes.Amber();
         IPalette frostPalette = new ScottPlot.Palettes.Frost();
-        
+
         double minValue = members.Min(x => x.PastSixWeeks - x.PrevSixWeeks);
         double maxValue = members.Max(x => x.PastSixWeeks - x.PrevSixWeeks);
 
@@ -107,10 +107,10 @@ public static class PlotService
 
             // Map the value to a color in the palette
             var normalizedValue = (value - minValue) / (maxValue - minValue);
-            var colorIndex = (int)(normalizedValue * 4); 
-            
+            var colorIndex = (int)(normalizedValue * 4);
+
             var barColor = value < 0 ? amberPalette.GetColor(colorIndex) : frostPalette.GetColor(colorIndex);
-            
+
             Bar bar = new(){ Value = value, Position = i, FillColor = barColor };
 
             plot.Add.Bar(bar);
@@ -126,12 +126,12 @@ public static class PlotService
         plot.FormatAxes();
 
         plot.Axes.Margins(bottom: .3, top: .3);
-        
+
         plot.YLabel("Change in Attendance");
         plot.Title("Change in Attendance");
-        
+
         var formattedDate = DateTime.Now.ToString("yyyy-MM-dd");
-        
+
         var filePath = Path.Combine(plotFolder, $"change_{formattedDate}.png");
 
         plot.SavePng(filePath, 1800, 800);
@@ -140,42 +140,75 @@ public static class PlotService
     private static void PlotMembersMeetingAttendance(List<Event> events, string plotFolder)
     {
         var plot = new Plot();
-        
+
         // ToDo - should use a tag for this
         var membersMeetings = events
             .Where(x => x.Tags.Contains("Members Meeting"))
-            .Where(x => x.MembersAttended.Count >0)
+            .Where(x => x.MembersAttended.Count > 0)
             .OrderByDescending(x => x.Date)
             .Reverse()
             .ToList();
-        
+
         var membersAttended = membersMeetings
             .Select((x, i) => new Coordinates(i, x.MembersAttended.Count))
             .ToList();
-        
+
         var ticks = membersMeetings
             .Select((x, i) => new Tick(i, x.Date.ToString("MMM yyyy")))
             .ToArray();
-        
+
         plot.Add.Scatter(membersAttended);
-        
+
         plot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(ticks);
-        
+
         plot.XLabel("Month");
         plot.YLabel("Members Attended");
         plot.Title("Members Meeting Attendance");
-        
+
         // Y Axis whole numbers only and start from 0   
         var tickGen = new ScottPlot.TickGenerators.NumericAutomatic{ IntegerTicksOnly = true };
         plot.Axes.Left.TickGenerator = tickGen;
-        
+
         var maxAttendance = membersMeetings.Select(x => x.MembersAttended.Count).Max();
         plot.Axes.SetLimits(0, membersMeetings.Count, 0, maxAttendance);
+
+        var formattedDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+        var filePath = Path.Combine(plotFolder, $"members_meeting_{formattedDate}.png");
+
+        plot.SavePng(filePath, 1800, 800);
+    }
+
+    // ToDo: Add gender splits
+    private static void PlotTemporalAttendance(List<Event> events, string plotFolder)
+    {
+        var filteredEvents = events.Where(e => e.MembersAttended.Count() >0).ToList();
+        var xs = filteredEvents.Select(e => e.Date.ToOADate()).ToArray();
+        var ys = filteredEvents.Select(e => (double)e.MembersAttended.Count).ToArray();
+        var labels = filteredEvents.Select(e => e.Name ?? "").ToArray();
+
+        var plot = new ScottPlot.Plot();
+
+        for (var i = 0; i < xs.Length; i++)
+        {
+            var x = xs[i];
+            var y = ys[i];
+            var label = labels[i];
+
+            var bar = plot.Add.Bar(x, y);
+        }
+
+        plot.Axes.DateTimeTicksBottom();
+        
+        plot.Title("Event Attendance Over Time");
+        plot.YLabel("Number of Attendees");
+        plot.XLabel("Date");
+        
+        plot.Axes.Margins(bottom: 0, top: .3);
         
         var formattedDate = DateTime.Now.ToString("yyyy-MM-dd");
-        
-        var filePath = Path.Combine(plotFolder, $"members_meeting_{formattedDate}.png");
-        
+        var filePath = Path.Combine(plotFolder, $"events_timeline_{formattedDate}.png");
+
         plot.SavePng(filePath, 1800, 800);
     }
 
@@ -186,9 +219,9 @@ public static class PlotService
         plot.Axes.Bottom.TickLabelStyle.Rotation = -45;
         plot.Axes.Bottom.TickLabelStyle.Alignment = Alignment.MiddleRight;
         plot.Axes.Bottom.MinimumSize = 200;
-        
+
         var tickGen = new ScottPlot.TickGenerators.NumericAutomatic{ IntegerTicksOnly = true };
-        plot.Axes.Left.TickGenerator  = tickGen;
+        plot.Axes.Left.TickGenerator = tickGen;
     }
 
     private static int GetAttendance(Member member, List<Event> events, int weeksAgoStart, int weeksAgoEnd)
